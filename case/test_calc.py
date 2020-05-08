907a94c42e625a11e18feb34b2804e525962e8c2
weiliang <1070555822@qq.com>
2020-05-08 12:10:45 +0800

    zhengshi

--- case/test_calc.py
+++ case/test_calc.py
@@ -1,17 +1,19 @@
 import pytest
+
 from util.Calc import Calc
 
 
-class test_calc:
-    def test_add_1(self):
+class Test_calc:
+    def setup(self):
         self.calc = Calc()
+
+    def test_add_1(self):
         rest = self.calc.add(1, 2)
-        assert 4 == rest
+        assert 3 == rest
 
     def test_div_1(self):
-        self.calc = Calc()
         rest = self.calc.div(1, 2)
-        assert 2 == rest
+        assert 1 == rest
 
 
 if __name__ == '__main__':
