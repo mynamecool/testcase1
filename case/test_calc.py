493dfc867b52db55288a29d25bec85460eed2064
weiliang <1070555822@qq.com>
2020-05-14 15:22:56 +0800

    zuizhong

--- case/test_calc.py
+++ case/test_calc.py
@@ -7,8 +7,9 @@ class Test_calc:
     def setup(self):
         self.calc = Calc()
 
+    @pytest.mark.second
     @pytest.mark.parametrize("a,b,c",
-                             [(1, 2, 3), (2, 3, 4), (None, None, None), (-1, 0, -1), (0.1, 0.1, 0.2), (0, 0, 0),
+                             [(1, 2, 3), (2, 3, 5), (None, None, None), (-1, 0, -1), (0.1, 0.1, 0.2), (0, 0, 0),
                               (9223372036854775807, 1, 9223372036854775808)])
     def test_add_1(self, a, b, c):
         try:
@@ -17,7 +18,7 @@ class Test_calc:
             assert b == None or a == None
         else:
             assert c == rest
-
+    @pytest.mark.first
     @pytest.mark.parametrize("a,b,c",
                              [(8, 4, 2), (-1, 1, -1), (1, -1, -1), (0.1, 0.5, 0.2), (1, 0, 1), (None, None, None)])
     def test_div_1(self, a, b, c):
